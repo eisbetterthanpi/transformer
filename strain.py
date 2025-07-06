34732c3f2afcaf29ec68ffeffb99ca5585c18734
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-07-07 07:41:29 +0800

    Update strain.py

--- strain.py
+++ strain.py
@@ -1,9 +1,11 @@
-# @title perplexity
+# @title train test generate
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-# https://www.comet.com/site/blog/perplexity-for-llm-evaluation/
+from torch.nn import functional as F
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+scaler = torch.GradScaler()
 
+# https://www.comet.com/site/blog/perplexity-for-llm-evaluation/
 def Perplexity(logits, target): # [b,t,vocab_size], [b,t]
     log_probs = F.log_softmax(logits, dim=-1)
     nll = -log_probs.gather(dim=-1, index=target.unsqueeze(-1)).squeeze(-1) # [b,t]
@@ -12,31 +14,25 @@ def Perplexity(logits, target): # [b,t,vocab_size], [b,t]
 
 # logits = torch.randn(2, 4, 10)
 # target = torch.tensor([[1, 2, 3, 4], [4, 3, 2, 1]])
-
 # perplexity = Perplexity(logits, target)
 # # perplexity = Perplexity(logits[:,:-1], y[:,1:])
 # print(f'Perplexity: {perplexity}')
-
-
-# @title train test generate
-import torch
-from torch.nn import functional as F
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
-scaler = torch.GradScaler()
 
 def strain(model, dataloader, optimizer, scheduler=None): # train function with automatic mixed precision
     model.train()
     for i, x in enumerate(dataloader):
         x, y = x[:,:-1], x[:,1:]
         x, y = x.to(device), y.to(device)
+
         with torch.autocast(device_type=device, dtype=torch.bfloat16):
             # causal_mask = torch.ones(x.size(1), x.size(1), dtype=bool, device=device).tril(diagonal=0).repeat(x.shape[0],1,1) # for F.scaled_dot_product_attention
             # # causal_mask = ~torch.ones(x.size(1), x.size(1), dtype=bool, device=device).tril(diagonal=0).repeat(x.shape[0],1,1)
             # logits = model(x, mask=causal_mask) #output = [batch size, trg len - 1, output dim]
             logits = model(x) #output = [batch size, trg len - 1, output dim]
             # logits, _ = model(x) # rnn
-            loss = F.cross_entropy(logits.flatten(0,1), y.flatten()) # [b*t,d], [b*t]
+            loss = F.cross_entropy(logits.flatten(0,1), y.flatten().to(int)) # [b*t,d], [b*t]
+            # loss = F.cross_entropy(logits.flatten(0,1), y.flatten()) # [b*t,d], [b*t]
+
         optimizer.zero_grad()
         scaler.scale(loss).backward()
         # scaler.unscale_(optim)
@@ -83,16 +79,14 @@ def generate(model, context, max_steps=64, temperature=1):
     completion = decode(x.squeeze(0))
     return completion
 
-import time
-start = begin = time.time()
+# import time
+# start = begin = time.time()
 for i in range(1):
 # for i in range(30):
     # train_loss = strain(model, train_loader, optim, scheduler=None)
     strain(model, train_loader, optim, scheduler=None)
     # test_loss = test(test_loader, model)
     # print('Test Loss:', test_loss)
-    print(generate(model, "this is what"))
-    print(i, 'time:',time.time() - start, (time.time()-begin)/(i+1))
-    start = time.time()
-
-
+    # print(generate(model, "this is what"))
+    # print(i, 'time:',time.time() - start, (time.time()-begin)/(i+1))
+    # start = time.time()
