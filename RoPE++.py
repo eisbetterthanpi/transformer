7fac0cefb36ed45ef4ab99279c2b9aa0977cad54
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-11-14 10:19:13 +0800

    Update RoPE++.py

--- RoPE++.py
+++ RoPE++.py
@@ -188,4 +188,37 @@ print(out.shape)
 
 
 
+class GoldenGateRoPE2d(nn.Module): # jerryxio.ng/posts/nd-rope
+    def __init__(self, image_size, n_heads, n_freqs):
+        super().__init__()
+        n_zero_freqs = 1
+        min_freq, max_freq = .2, 20
+        intv = math.pi * (math.sqrt(5)-1)/2
+        speed = torch.cat([torch.zeros(n_zero_freqs), min_freq * (max_freq/min_freq) ** torch.linspace(0,1,n_freqs-n_zero_freqs)]) # [n_freqs]
+        phi = torch.arange(n_heads*n_freqs).reshape(n_heads, n_freqs) * intv # [n_heads, n_freqs]
+        direction = torch.stack((torch.cos(phi), torch.sin(phi)), dim=-1) # [n_heads, n_freqs, 2]
+        vel = speed.unsqueeze(-1) * direction # speed in direction[n_heads, n_freqs, 2]
+        h, w = image_size
+        xlim, ylim = math.sqrt(w/h), math.sqrt(h/w)
+        y, x = torch.meshgrid(torch.linspace(-ylim, ylim, h), torch.linspace(-xlim, xlim, w), indexing="ij") # [h,w], y:row_num, x:col_num
+        pos = torch.stack([x, y], dim=-1)[...,None,None,:] # [h,w,1,1,2] cartesian coords
+        theta = (vel*pos).sum(dim=-1) # [h,w,n_heads,n_freqs,2]->[h,w,n_heads,d_head]
+        cos, sin = torch.cos(theta), torch.sin(theta)
+        self.affine = torch.stack([cos, -sin, sin, cos], dim=-1).unflatten(-1, (2,2))
+
+    def forward(self, input): # [b,h,w,n_head,d_head]
+        return (self.affine @ input.unflatten(-1, (-1,2)).unsqueeze(-1)).flatten(-3)
+
+# image_size=(5,7)
+# n_heads=4
+# d_head=16
+# ggrope = GoldenGateRoPE2d(image_size, n_heads, d_head//2)
+
+# x = torch.rand(2, *image_size, n_heads, d_head)
+# out = ggrope(x)
+# print(out.shape)
+
+
+
+
 
